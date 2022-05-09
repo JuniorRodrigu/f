c394578da7e558ebcbce72b8d0ba843907a65b3e
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2022-05-09 15:34:06 -0300

    Projeto criado

---
+++ beckend/src/main/java/com/dev/dvmove/DvmoveApplication.java
@@ -0,0 +1,13 @@
+package com.dev.dvmove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvmoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvmoveApplication.class, args);
+	}
+
+}
